5d2fa973cb136ddba68747a11abadde9d8477b94
PsioNicolas <nicolas@banatt.com>
2026-05-25 01:13:09 -0400

    Updates

---
+++ _app/immutable/nodes/2.CEJsuBos.js
@@ -0,0 +1,10 @@
+import{E as e,O as t,S as n,h as r,m as i,s as a,u as o}from"../chunks/lKAUG57D.js";import"../chunks/xihTtKlq.js";import{t as s}from"../chunks/q812xlt6.js";function c(c){a(`1hp6taq`,t=>{n(()=>{e.title=`Home`})}),s(c,{children:(e,n)=>{var a=r();o(t(a),()=>`<pre class="shiki github-dark" style="background-color:#24292e;color:#e1e4e8" tabindex="0"><code><span class="line"><span style="color:#6A737D">-- Lua was my first programming language :D (PICO-8 &#x3C;3)</span></span>
+<span class="line"><span style="color:#F97583">local</span><span style="color:#F97583"> function</span><span style="color:#B392F0"> welcome</span><span style="color:#E1E4E8">()</span></span>
+<span class="line"><span style="color:#79B8FF">    print</span><span style="color:#E1E4E8">(</span><span style="color:#9ECBFF">[[</span></span>
+<span class="line"><span style="color:#9ECBFF">This is the website of Nicolas Banatt, also known as PsioNick on the internet. This site is mainly for documenting my projects, both academic and personal.</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#9ECBFF">My personal interests include programming, game development, speedrunning, anime and manga.</span></span>
+<span class="line"><span style="color:#9ECBFF">    ]]</span><span style="color:#E1E4E8">)</span></span>
+<span class="line"><span style="color:#F97583">end</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#79B8FF">welcome</span><span style="color:#E1E4E8">()</span></span></code></pre>`),i(e,a)},$$slots:{default:!0}})}export{c as component};
